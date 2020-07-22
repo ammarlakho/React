0159e416f3fd637e9bd8c10f2b9df310cd1ea658
ammarlakho <ammar1lakho@gmail.com>
2020-07-22 20:00:59 +0500

    React-Redux-Form Revisited

---
+++ src/redux/forms.js
@@ -0,0 +1,11 @@
+const { initialFieldState } = require("react-redux-form")
+
+export const InitialFeedback = {
+    firstname: '',
+    lastname: '',
+    telnum: '',
+    email: '',
+    agree: false,
+    contactType: 'Tel.',
+    message: ''
+};
